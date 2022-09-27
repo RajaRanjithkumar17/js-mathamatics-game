20a7f95989245d7aaec1a6c26e88951c4d2450af
RajaRanjithkumar17 <rajaranjithkumar17.com>
2022-09-27 16:36:19 +0530

    my commit

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var startBtn = document.getElementById("start");
 var dispProb = document.getElementsByClassName("containin")[0];
-
-
+var checkResult = document.getElementById("checkResult"),icmnt=0, userResult;
 
  var q=2;
 function genRandomnumber(q){
@@ -27,10 +26,9 @@ function identOperator(identOperat){
 
    }
 
-
 function updateTouser(ranNum1,ranNum2,getoperationEle){
     var  dispDiv = document.createElement("div");
-    dispDiv.setAttribute("class","containinput");
+    dispDiv.setAttribute("id","containinput");
 
     var  dispDivstyle = document.createElement("div");
     dispDivstyle.setAttribute("class","flex-box");
@@ -45,7 +43,7 @@ function updateTouser(ranNum1,ranNum2,getoperationEle){
     dispnum2.innerText = ranNum2;
 
     var inputEle = document.createElement("input");
-    inputEle.setAttribute("class","answer");
+    inputEle.setAttribute("id","answer"+icmnt);
     inputEle.setAttribute("type","text");
    
     dispDiv.appendChild(dispnum1);
@@ -54,10 +52,7 @@ function updateTouser(ranNum1,ranNum2,getoperationEle){
     dispDiv.appendChild(inputEle);
     dispDivstyle.appendChild(dispDiv)
     dispProb.appendChild(dispDivstyle);
-    
-    
-
-   
+ 
 
 }
 
@@ -67,25 +62,91 @@ function updateTouser(ranNum1,ranNum2,getoperationEle){
     var rangeEle = document.getElementById("range");
     var getoperationEle = operationEle.value ;
     var getrangeEle = parseInt( rangeEle.value);//
-    var ranNum1,ranNum2,range;
+    var ranNum1,ranNum2;
+    // ranNum1.setAttribute("class", "rannum1")
+    // ranNum2.setAttribute("class", "rannum2")
 
     console.log(getrangeEle);
     console.log(getoperationEle );
 
     //gen random num
 
-    if(getoperationEle!=="division"){
+    if(getoperationEle!==" "){
         for(let i=1; i<=16; i++){
             var ranNum1 = genRandomnumber(getrangeEle);
             var ranNum2 = genRandomnumber(getrangeEle);
             updateTouser(ranNum1,ranNum2,getoperationEle);
+            var arry= [];
+            var result;
+           
+            arry.push(result)
+            console.log(arry);
+            
+           if(getoperationEle=="add"){
+            result =ranNum1+ranNum2;
+            arry.push(result)
+           }
+           if(getoperationEle=="sub"){
+            result =ranNum1-ranNum2;
+            arry.push(result)
+           }
+           if(getoperationEle=="multiple"){
+            result =ranNum1*ranNum2;
+            arry.push(result)
+           }
+           if(getoperationEle=="division"){
+            result =ranNum1/ranNum2;
+            arry.push(result)
+           }
             
 
         }
+       
+    }
+    checkResult.addEventListener("click",()=>{
+        {
+            var userRes = document.getElementById("answer"+icmnt);
+            var userResultcheck = document.getElementById("containinput");
+            console.log(userRes);
+            console.log(userResultcheck);
+            
+            var userinp=[];
+            for(a=1;a<=16;a++){            // pending
+                if(getoperationEle==" "){  //
+                    
+                    userinp.push(userRes.value) //
+
+                }
+                icmnt++; //
+                console.log(icmnt); //
+            }
+            
+            console.log(userinp);
+            
+
+
+
+
+
+        //     finalresult.innerText = userResult.value;
+        //   console.log(finalresult);
+
+            if(userinp[0]==arry[0]){
+                userResultcheck.style.border;"1px solid green";
+            }
+            else{
+                userResultcheck.style.border;"  1px solid red";
+            }
+        
+    
+    }});
 
     } 
-    
- }
-    
 
- startBtn.addEventListener("click",userInp)
+ startBtn.addEventListener("click",userInp);
+
+
+ 
+
+
+
