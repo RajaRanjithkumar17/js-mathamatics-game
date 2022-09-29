c489ca425d5f358cc17267944a87fa194f0512b6
RajaRanjithkumar17 <rajaranjithkumar17.com>
2022-09-29 19:18:33 +0530

    m

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var startBtn = document.getElementById("start");
 var dispProb = document.getElementsByClassName("containin")[0];
-var checkResult = document.getElementById("checkResult"),icmnt=0, userResult;
+var checkResult = document.getElementById("checkResult");
 
  var q=2;
 function genRandomnumber(q){
@@ -35,15 +35,18 @@ function updateTouser(ranNum1,ranNum2,getoperationEle){
 
     var dispnum1 = document.createElement("h3");
     dispnum1.innerText = ranNum1;
+    dispnum1.setAttribute("class","dispNum1")
 
     var dispoperator = document.createElement("h3");
     dispoperator.innerText = identOperator(getoperationEle);
+    dispoperator.setAttribute("class","dispOpEle")
 
     var dispnum2 = document.createElement("h3");
     dispnum2.innerText = ranNum2;
+    dispnum2.setAttribute("class","dispNum2")
 
     var inputEle = document.createElement("input");
-    inputEle.setAttribute("id","answer"+icmnt);
+    inputEle.setAttribute("class","answer");
     inputEle.setAttribute("type","text");
    
     dispDiv.appendChild(dispnum1);
@@ -54,8 +57,8 @@ function updateTouser(ranNum1,ranNum2,getoperationEle){
     dispProb.appendChild(dispDivstyle);
  
 
-}
 
+}
 
  const userInp = function(){
     var operationEle = document.getElementById("operation");
@@ -71,33 +74,12 @@ function updateTouser(ranNum1,ranNum2,getoperationEle){
 
     //gen random num
 
-    if(getoperationEle!==" "){
-        for(let i=1; i<=16; i++){
+    if(getoperationEle!=="division"){
+        for(let i=0; i<=16; i++){
             var ranNum1 = genRandomnumber(getrangeEle);
             var ranNum2 = genRandomnumber(getrangeEle);
             updateTouser(ranNum1,ranNum2,getoperationEle);
-            var arry= [];
-            var result;
            
-            arry.push(result)
-            console.log(arry);
-            
-           if(getoperationEle=="add"){
-            result =ranNum1+ranNum2;
-            arry.push(result)
-           }
-           if(getoperationEle=="sub"){
-            result =ranNum1-ranNum2;
-            arry.push(result)
-           }
-           if(getoperationEle=="multiple"){
-            result =ranNum1*ranNum2;
-            arry.push(result)
-           }
-           if(getoperationEle=="division"){
-            result =ranNum1/ranNum2;
-            arry.push(result)
-           }
             
 
         }
@@ -105,45 +87,94 @@ function updateTouser(ranNum1,ranNum2,getoperationEle){
     }
     checkResult.addEventListener("click",()=>{
         {
-            var userRes = document.getElementById("answer"+icmnt);
-            var userResultcheck = document.getElementById("containinput");
-            console.log(userRes);
-            console.log(userResultcheck);
-            
-            var userinp=[];
-            for(a=1;a<=16;a++){            // pending
-                if(getoperationEle==" "){  //
-                    
-                    userinp.push(userRes.value) //
 
-                }
-                icmnt++; //
-                console.log(icmnt); //
-            }
-            
-            console.log(userinp);
-            
+            var num1Ele,num2Ele,operationEle,operationEleValue ,actualAns,expectAns,
+            num1EleValue, num2EleValue,actualAnswer,aaa,mark=0;
+            // 1.get num 1 and num2 value
 
 
-
-
-
-        //     finalresult.innerText = userResult.value;
-        //   console.log(finalresult);
-
-            if(userinp[0]==arry[0]){
-                userResultcheck.style.border;"1px solid green";
-            }
-            else{
-                userResultcheck.style.border;"  1px solid red";
-            }
+            //3.find expect answer
+                  
         
+            
+         for(i=0;i<16;i++){
+
+            
+            num1Ele = document.getElementsByClassName("dispNum1")[i];
+            
+            num1EleValue = parseInt(num1Ele.innerText);
+            console.log(num1EleValue);
+
+            num2Ele = document.getElementsByClassName("dispNum2")[i];
+            num2EleValue = parseInt(num2Ele.innerText);
+            console.log(num2EleValue);
+
+            2// find operation
+
+            operationEle = document.getElementsByClassName("dispOpEle")[i];
+            operationEleValue = operationEle.innerText;
+
+            console.log(operationEleValue );
+
+
+
+
+          //  var arry= [];
+           
+            //arry.push(expectAns)
+            // console.log(arry); //
+             console.log(getoperationEle); //
+
+
+            if(getoperationEle =="add"){
+                expectAns =num1EleValue + num2EleValue ;
+
+                var arry= [];
+                console.log(arry);//
+                
+                arry.push(expectAns)
+               }
+              else if(getoperationEle=="sub"){
+                expectAns =num1EleValue - num2EleValue ;
+                arry.push(expectAns)
+               }
+             else  if(getoperationEle=="multiple"){
+                expectAns =num1EleValue * num2EleValue ;
+                arry.push(expectAns)
+               }
+         
+       
+
+
+        //4.get actual value
+         var ans=[];
+        actualAns = document.getElementsByClassName("answer")[0];
+         aaa = parseInt(actualAns.value)
+
+
+        ans.push(aaa);
+        console.log(ans);
+
+        
+   
+
+
+            }
+                   //5.check answwwwwwwer
+        //   if(ans[i]==arry[i]){
+        //     mark = mark+1;
+        //     alert(mark)
+        // }
+
+      
+          
     
     }});
 
     } 
+    startBtn.addEventListener("click",userInp);
 
- startBtn.addEventListener("click",userInp);
+
 
 
  
